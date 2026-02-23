ae9ee712adcece9c9b112ba5354e2df595fed979
Andrey <andrey.virts@mxbox.link>
2026-02-23 15:54:21 +0100

    feat(angular major update): update @angular/core@21 @angular/cli@21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { RouterOutlet } from '@angular/router';
 import { TimerComponent } from './components/timer/timer.component';
 import { TasksComponent } from './components/tasks/tasks.component';
@@ -7,11 +7,10 @@ import { TasksComponent } from './components/tasks/tasks.component';
 @Component({
     selector: 'app-root',
     imports: [
-        CommonModule,
-        RouterOutlet,
-        TimerComponent,
-        TasksComponent
-    ],
+    RouterOutlet,
+    TimerComponent,
+    TasksComponent
+],
     template: `
     <div class="container mat-elevation-z2">
       <h1 class="mat-h1">{{ title }}</h1>
